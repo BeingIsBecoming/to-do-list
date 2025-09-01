325ff36393c3a56201c31c4228f3aec292fa6467
beingisbecoming <129126782+BeingIsBecoming@users.noreply.github.com>
2025-08-31 18:20:05 -0700

    Update button placement

--- src/index.js
+++ src/index.js
@@ -2,9 +2,5 @@ import "./styles.css";
 import { greeting } from "./greeting.js";
 import todaysDate from "./date.js";
 
-// const image = document.createElement("img");
-// image.src = somaImage;
-// document.body.appendChild(image);
-
 document.getElementById("date-today").appendChild(todaysDate());
 console.log(greeting);
