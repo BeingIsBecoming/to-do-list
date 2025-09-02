0e0cbd35968fd4fbd460e81dbde7a357113e33f5
beingisbecoming <129126782+BeingIsBecoming@users.noreply.github.com>
2025-09-02 16:50:30 -0700

    Create ToDo class.

---
+++ todo.js
@@ -0,0 +1,10 @@
+class ToDo {
+    constructor(title,project,description,dueDate) {
+        this.title = title;
+        this.project = project;
+        this.description = description;
+        this.dueDate = dueDate;
+    }
+
+
+}
