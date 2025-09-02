c3397d6e91868d7e52487e959a14e66fc950d604
beingisbecoming <129126782+BeingIsBecoming@users.noreply.github.com>
2025-09-01 17:30:08 -0700

    Create addTask module with alert check.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import "./styles.css";
-import { greeting } from "./greeting.js";
 import todaysDate from "./date.js";
+import createTask from "./task.js";
 
 document.getElementById("date-today").appendChild(todaysDate());
-console.log(greeting);
+
+// Call Modules
+createTask();
---
+++ src/task.js
@@ -0,0 +1,7 @@
+export default function createTask() {
+    const addTaskButton = document.querySelector('.add-task');
+    
+    addTaskButton.addEventListener('click', (e) => {
+        alert("This is the add task form checker");
+    });
+}
