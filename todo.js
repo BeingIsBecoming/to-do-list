bfb8120a4c87bbf568a5963c925febb095b97d6b
beingisbecoming <129126782+BeingIsBecoming@users.noreply.github.com>
2025-09-03 13:45:00 -0400

    Create Task extends Project class redesign.

--- todo.js
+++ todo.js
@@ -1,10 +1,13 @@
-class ToDo {
-    constructor(title,project,description,dueDate) {
+class Project {
+    static allTasks = [];
+    
+    constructor(title,description,dueDate) {
         this.title = title;
-        this.project = project;
         this.description = description;
         this.dueDate = dueDate;
     }
 
-
+}
+class Task extends Project {
+    constructor()
 }
